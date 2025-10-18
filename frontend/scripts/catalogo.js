f217dd2dff46a6877de8fd747c997e1e1d938d3b
ricardo <richisanj@gmail.com>
2025-10-18 13:29:24 -0400

    Add cart page and integrate buy button functionality in catalog

--- frontend/scripts/catalogo.js
+++ frontend/scripts/catalogo.js
@@ -2,13 +2,14 @@
 document.querySelectorAll(".buy-btn").forEach(btn => {
   btn.addEventListener("click", () => {
     const product = {
+      id: btn.dataset.id,          // <--- guardar ID
       name: btn.dataset.product,
       price: Number(btn.dataset.price),
       quantity: 1
     };
 
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    const existing = cart.find(p => p.name === product.name);
+    const existing = cart.find(p => p.id === product.id);
     if (existing) {
       existing.quantity += 1;
     } else {
